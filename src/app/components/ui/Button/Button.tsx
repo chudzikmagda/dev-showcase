49cfb62d24ef448293ba7ecaa7ec303ddff95540
magda.chudzik <kontakt@magdachudzik.pl>
2026-04-01 22:10:10 +0200

    feat(home page): Add intro section

---
+++ src/app/components/ui/Button/Button.tsx
@@ -0,0 +1,90 @@
+"use client";
+import styled from "styled-components";
+import { ButtonProps, ButtonType } from "../models/button.model";
+
+const getButtonColor = (version: ButtonType) => {
+  switch (version) {
+    case ButtonType.PRIMARY:
+      return "var(--primary-color)";
+    case ButtonType.SECONDARY:
+      return "var(--black)";
+    default:
+      return undefined;
+  }
+};
+
+const getHoverColor = (version: ButtonType) => {
+  switch (version) {
+    case ButtonType.PRIMARY:
+      return "var(--white)";
+    case ButtonType.SECONDARY:
+      return "var(--primary-color)";
+    default:
+      return undefined;
+  }
+};
+
+const ButtonWrapper = styled.button<{
+  version: ButtonType;
+  $hasArrow: boolean;
+}>`
+  all: unset;
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+  margin: 1rem 0;
+  font-weight: var(--font-weight-bold);
+  font-size: 1rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${({ version }) => getButtonColor(version)};
+  transition:
+    color var(--transition-default),
+    transform var(--transition-default);
+  cursor: pointer;
+
+  &:link,
+  &:visited {
+    text-decoration: none;
+    line-height: 1.25;
+  }
+
+  &::after {
+    content: "";
+    width: 8px;
+    height: 22px;
+    display: ${({ $hasArrow }) => ($hasArrow ? "inline-block" : "none")};
+    background-color: ${({ version }) => getButtonColor(version)};
+    mask-image: url("/images/bracets/arrow-next-small.svg");
+    mask-repeat: no-repeat;
+    mask-size: contain;
+    transition: background-color var(--transition-default);
+  }
+
+  &:hover,
+  &:focus-visible {
+    color: ${({ version }) => getHoverColor(version)};
+    transform: scale(1.05);
+
+    &::after {
+      background-color: ${({ version }) => getHoverColor(version)};
+    }
+  }
+
+  &:focus-visible {
+    padding: 0 8px;
+    margin: 2rem 4px;
+  }
+`;
+
+const Button = ({ label, version, hasArrow, onClick }: ButtonProps) => {
+  return (
+    <ButtonWrapper version={version} $hasArrow={hasArrow} onClick={onClick}>
+      {label}
+    </ButtonWrapper>
+  );
+};
+
+export default Button;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
-import * as React from "react";
 import styled from "styled-components";
+import Intro from "./components/layout/Intro/Intro";
 
 const Container = styled.div`
   background-color: var(--secondary-color);
   height: 100vh;
 `;
 export default function Home() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Intro></Intro>
+    </Container>
+  );
 }
