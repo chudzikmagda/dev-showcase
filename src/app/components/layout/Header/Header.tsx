96d01ade81c9252406c6a00c8b2b856a8e334214
magda.chudzik <kontakt@magdachudzik.pl>
2026-05-08 16:26:05 +0200

    feat: Make a logotype collapsible on scroll

--- src/app/components/layout/Header/Header.tsx
+++ src/app/components/layout/Header/Header.tsx
@@ -1,12 +1,29 @@
 "use client";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Logotype from "../../ui/Logotype/Logotype";
 import MainNavigation from "../MainNavigation/MainNavigation";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const nextIsScrolled = window.scrollY > 0;
+      setIsScrolled((prev) =>
+        prev !== nextIsScrolled ? nextIsScrolled : prev,
+      );
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <HeaderWrapper>
-      <Logotype></Logotype>
+      <Logotype isScrolled={isScrolled}></Logotype>
       <MainNavigation></MainNavigation>
     </HeaderWrapper>
   );
