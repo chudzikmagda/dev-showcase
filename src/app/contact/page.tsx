53e9998bde2f142f7cb414503efb0e80443d6811
magda.chudzik <kontakt@magdachudzik.pl>
2026-06-24 16:32:27 +0200

    feat: Add Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { JSX } from "react";
+
+import { H1Heading, PageWrapper } from "./contact.styles";
+
+export const metadata: Metadata = {
+  title: "Contact | Magda Chudzik",
+  description:
+    "Get in touch with Magda Chudzik for frontend and UI/UX project inquiries.",
+};
+
+const ContactPage = (): JSX.Element => {
+  return (
+    <PageWrapper>
+      <H1Heading>Let&apos;s get in touch</H1Heading>
+    </PageWrapper>
+  );
+};
+
+export default ContactPage;
